22fec94583ead36a25a8fe92b5f7769be2a72111
youths <552099787@qq.com>
2017-06-03 10:46:41 +0800

    second commit

---
+++ src/com/game/test/LifeGameTest.java
@@ -0,0 +1,15 @@
+package com.game.test;
+
+import org.junit.Test;
+
+/**
+ * Created by Think on 2017/6/3.
+ */
+public class LifeGameTest {
+    //期望值
+
+    @Test
+    public void printLifeGameGraph(){
+
+    }
+}
---
+++ src/com/game/util/LifeGame.java
@@ -0,0 +1,51 @@
+package com.game.util;
+
+/**
+ * Created by Think on 2017/6/3.
+ */
+//定义：*代表alive  space代表dead
+public class LifeGame {
+    //自己给好的一个初始化状态
+    public char[][] init(){
+            char[][] graph = new char[3][3];
+            return graph;
+    }
+    public char[][] expendInit(char[][] graph){
+        char[][] graphIncrease = new char[graph.length+1][graph.length+1];
+        for (int row= 0; i < graphIncrease.length; i++){
+            for (int col = 0; col < graphIncrease[row].length; col++){
+                if((row == 0) || (col == 0 ) || (row == graphIncrease.length-1) || (col == graphIncrease[row].length-1){
+                    graphIncrease[row][col]=' ';
+                }
+            }
+        }
+        return graph;
+    }
+
+    public char[][] gameContinue(char[][] graph){
+        //记录活着的细胞数count
+        int count = 0;
+        for(int index = 0; index<graph.length; index++){
+            for (int tab = 0; tab< graph[index].length; tab++){
+                //左上角
+                if((index == 0) &&(tab == 0)){
+                    if(graph[index][tab] == '*'){
+                        if(graph[index][tab+1] == '*'){
+                            count++;
+                        }
+                        if(graph[index+1][tab+1]=='*'){
+                            count++;
+                        }
+                        if(graph[index+1][tab] =='*'){
+                            count++;
+                        }
+                        if(count > )
+                    }else{
+
+                    }
+                }
+            }
+        }
+        return graph;
+    }
+}
